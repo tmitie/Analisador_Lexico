92b93b69d76aeaea0cfd96dd767009000413e548
Thaynara Mitie <thaynara.mitie@outlook.com>
2019-09-30 13:39:59 -0300

    1.0 First Commit

--- Analisador_Lexico_/Form1.Designer.cs
+++ Analisador_Lexico_/Form1.Designer.cs
@@ -28,33 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bt_Browse = new System.Windows.Forms.Button();
+            this.textBox_File = new System.Windows.Forms.TextBox();
+            this.bt_Analize = new System.Windows.Forms.Button();
+            this.richTextBox_File = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
-            // button1
+            // bt_Browse
             // 
-            this.button1.Location = new System.Drawing.Point(713, 13);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.bt_Browse.Location = new System.Drawing.Point(713, 13);
+            this.bt_Browse.Name = "bt_Browse";
+            this.bt_Browse.Size = new System.Drawing.Size(75, 23);
+            this.bt_Browse.TabIndex = 0;
+            this.bt_Browse.Text = "Browse";
+            this.bt_Browse.UseVisualStyleBackColor = true;
+            this.bt_Browse.Click += new System.EventHandler(this.bt_abrir_arquivo_Click);
+            // 
+            // textBox_File
+            // 
+            this.textBox_File.Location = new System.Drawing.Point(12, 417);
+            this.textBox_File.Multiline = true;
+            this.textBox_File.Name = "textBox_File";
+            this.textBox_File.Size = new System.Drawing.Size(695, 21);
+            this.textBox_File.TabIndex = 1;
+            this.textBox_File.Visible = false;
+            // 
+            // bt_Analize
+            // 
+            this.bt_Analize.Location = new System.Drawing.Point(713, 42);
+            this.bt_Analize.Name = "bt_Analize";
+            this.bt_Analize.Size = new System.Drawing.Size(75, 23);
+            this.bt_Analize.TabIndex = 2;
+            this.bt_Analize.Text = "Analize";
+            this.bt_Analize.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox_File
+            // 
+            this.richTextBox_File.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox_File.Name = "richTextBox_File";
+            this.richTextBox_File.Size = new System.Drawing.Size(695, 399);
+            this.richTextBox_File.TabIndex = 3;
+            this.richTextBox_File.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox_File);
+            this.Controls.Add(this.bt_Analize);
+            this.Controls.Add(this.textBox_File);
+            this.Controls.Add(this.bt_Browse);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Compiladores: Analisador Léxico";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button bt_Browse;
+        private System.Windows.Forms.TextBox textBox_File;
+        private System.Windows.Forms.Button bt_Analize;
+        private System.Windows.Forms.RichTextBox richTextBox_File;
     }
 }
 
--- Analisador_Lexico_/Form1.cs
+++ Analisador_Lexico_/Form1.cs
@@ -1,20 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
+using System.Globalization;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace Analisador_Lexico_
 {
     public partial class Form1 : Form
     {
+        private string arquivo;
+        private string mensagem;
+        List<string> mensagemLinha = new List<string>();
+        OpenFileDialog OFD = new OpenFileDialog();
+        StreamReader SR;
+        string fileName = null;
+        string path = null;
+        string instructionLine = null;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        private void bt_abrir_arquivo_Click(object sender, EventArgs e)
+        {
+            OFD.Filter = "Arquivo texto (*.txt)|*.txt";
+            OFD.ShowDialog();
+
+            if (string.IsNullOrEmpty(OFD.FileName) == false)
+            {
+                try
+                {
+                    using (SR = new StreamReader(OFD.FileName))
+                    {
+
+                        textBox_File.Text = SR.ReadToEnd().Trim();
+                        richTextBox_File.Text = textBox_File.Text;
+                        instructionLine = textBox_File.Text;
+                        path = OFD.FileName;
+                        fileName = Path.GetFileName(OFD.FileName);
+
+                    }
+
+                }
+                catch
+                {
+
+                }
+            }
         }
     }
 }
